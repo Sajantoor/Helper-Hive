d7e9e27f67b9b77289dcbe6a078a4cf037ce8670
Sajan Toor <sajantoor98@gmail.com>
2024-08-05 00:45:39 -0700

    Create calendar event

---
+++ client/src/lib/Components/CalendarEvent.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	export let img: string = '';
+	export let title: string = '';
+	export let location: string = '';
+	export let date: Date = new Date();
+
+	const iconSize = 25;
+</script>
+
+<div class="calendarEvent max-w-80 w-full h-1/4 m-5 border-white border-2">
+	<img src={img} alt={title} class="h-36 w-full object-cover" />
+	<div class="p-2 flex justify-between items-end">
+		<div class="inline-block mr-6">
+			<p class="text-l">{date.toLocaleDateString('en-US', { weekday: 'short' }).toUpperCase()}</p>
+			<p class="text-2xl font-bold">{date.getDate()}</p>
+		</div>
+		<div class="inline-block flex-grow">
+			<p>{location}</p>
+			<p>
+				{date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true })}
+			</p>
+			<p class="font-bold">{title}</p>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	.calendarEvent {
+		border-color: #f4f4f4;
+	}
+</style>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import EventCard from '$lib/Components/EventCard.svelte';
+	import CalendarEvent from '$lib/Components/CalendarEvent.svelte';
 </script>
 
 <EventCard
@@ -9,4 +10,19 @@
 	organization="The City of Vancouver"
 	location="Vancouver, BC 1455 Quebec St"
 	organizationLogo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT46Pp3V6MJmwjab7ghq7TSOu5COQongTQ83Q&s"
+/>
+
+<CalendarEvent
+	img="https://assets.simpleviewinc.com/simpleview/image/upload/crm/vancouverbc/Science-World-03-large_7A18828C-0A13-47CD-92DB4404AE5A222C_fb32edf9-a996-4f5f-a2aaa1980fe8d6b7.jpg"
+	title="Science World Children's Day"
+	date={new Date()}
+	location="Vancouver, BC 1455 Quebec St"
+/>
+
+<!-- make the date today + a day -->
+<CalendarEvent
+	img="https://assets.simpleviewinc.com/simpleview/image/upload/crm/vancouverbc/Science-World-03-large_7A18828C-0A13-47CD-92DB4404AE5A222C_fb32edf9-a996-4f5f-a2aaa1980fe8d6b7.jpg"
+	title="Science World Children's Day way longer"
+	date={new Date(new Date().getTime() + 26 * 24 * 60 * 60 * 1000)}
+	location="Vancouver, BC 1455 Quebec Stdfadsfl;kjadskf jkda;fjds"
 />
