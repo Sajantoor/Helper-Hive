0d5a06408dcd61601905ed6ac558ac72cd234db7
Sajan Toor <sajantoor98@gmail.com>
2024-08-12 10:19:11 -0700

    Finish events scroller

--- client/src/lib/Components/EventScroller.svelte
+++ client/src/lib/Components/EventScroller.svelte
@@ -2,12 +2,16 @@
 	import EventCard from '$lib/Components/EventCard.svelte';
 	import type { Event } from '$lib/Types/Events';
 
+	import ArrowLeft from 'svelte-material-icons/ArrowLeft.svelte';
+	import ArrowRight from 'svelte-material-icons/ArrowRight.svelte';
+
 	export let events: Event[] = [];
 	let currentSlide = 0;
 
-	function nextSlide() {
-		const totalSlides = Math.ceil(events.length / 4); // Assuming 4 cards per view
+	const NUM_CARDS_PER_SLIDE = 4;
+	const totalSlides = Math.ceil(events.length / NUM_CARDS_PER_SLIDE); // Assuming 4 cards per view
 
+	function nextSlide() {
 		if (currentSlide < totalSlides - 1) {
 			currentSlide++;
 		}
@@ -21,7 +25,11 @@
 </script>
 
 <div class="carouselContainer">
-	<button class="prevButton" on:click={prevSlide}>&lt;</button>
+	{#if currentSlide > 0}
+		<button class="prevButton" on:click={prevSlide}>
+			<ArrowLeft />
+		</button>
+	{/if}
 	<div class="eventScroller" style="transform: translateX(calc(-100% * {currentSlide}));">
 		{#each events as event, i}
 			<EventCard
@@ -34,7 +42,18 @@
 			/>
 		{/each}
 	</div>
-	<button class="nextButton" on:click={nextSlide}>&gt;</button>
+
+	{#if currentSlide < totalSlides - 1}
+		<button class="nextButton" on:click={nextSlide}>
+			<ArrowRight />
+		</button>
+	{/if}
+</div>
+
+<div class="indicatorDots">
+	{#each Array.from({ length: totalSlides }) as _, i}
+		<button class="dot" class:active={i === currentSlide} on:click={() => (currentSlide = i)} />
+	{/each}
 </div>
 
 <style>
@@ -48,20 +67,27 @@
 		display: flex;
 		scroll-behavior: smooth;
 		width: 100%;
-		transition: transform 0.3s ease-in-out; /* Smooth transition */
+		transition: transform 0.3s ease-in-out;
 	}
 
 	.prevButton,
 	.nextButton {
-		background-color: #fff;
 		border: none;
-		cursor: pointer;
-		font-size: 2rem;
 		padding: 10px;
 		position: absolute;
 		top: 50%;
+		border-radius: 50%;
 		transform: translateY(-50%);
 		z-index: 1;
+		transition:
+			background-color 0.3s,
+			box-shadow 0.3s;
+	}
+
+	.prevButton:hover,
+	.nextButton:hover {
+		background-color: rgba(255, 255, 255, 1);
+		box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
 	}
 
 	.prevButton {
@@ -70,5 +96,23 @@
 
 	.nextButton {
 		right: 0;
+	}
+
+	.indicatorDots {
+		display: flex;
+		justify-content: center;
+	}
+
+	.dot {
+		background-color: #ffe6a6;
+		border-radius: 50%;
+		width: 10px;
+		height: 10px;
+		margin: 0 5px;
+		transition: background-color 0.3s;
+	}
+
+	.active.dot {
+		background-color: #fabd22;
 	}
 </style>
