9b108653f6e34dcdb9755e24d527b62d87fb749f
Sajan Toor <sajantoor98@gmail.com>
2024-08-16 17:26:23 -0700

    Almost finish top profile section

--- client/src/routes/profile/+page.svelte
+++ client/src/routes/profile/+page.svelte
@@ -1,27 +1,46 @@
 <script lang="ts">
 	import NavBar from '$lib/Components/NavBar.svelte';
+	import Section from '$lib/Components/Text/Section.svelte';
 	import Text from '$lib/Components/Text/Text.svelte';
 	import Pencil from 'svelte-material-icons/Pencil.svelte';
 
 	let profilePicSrc = 'https://cdn-icons-png.freepik.com/512/4032/4032866.png'; // www.freepik.com
 	let name = 'Jane Doe';
 	let isCurrentUser = true;
+	let bio =
+		'Adventurous soul, avid learner, and always up for a good laugh. Living life one moment at a time 100.';
 </script>
 
 <NavBar />
 
-<div class="flex">
-	<img src={profilePicSrc} alt="Profile" class="h-32 w-32 mr-10 rounded-full object-cover" />
-	<Text>{name}</Text>
+<div class="ml-8 mt-2 p-8 pb-28">
 	{#if isCurrentUser}
-		<div class="editProfile absolute right-8">
-			<button class="flex">
+		<div class="editProfile float-right">
+			<button class="editProfile flex border rounded-full p-2 pl-4 pr-4">
 				<Pencil size={25} />
-				<Text>Edit Profile</Text>
+				<Text class="ml-2">Edit Profile</Text>
 			</button>
 		</div>
 	{/if}
+
+	<img
+		src={profilePicSrc}
+		alt="Profile"
+		class="h-40 w-40 mr-10 rounded-full object-cover float-left"
+	/>
+	<Section class="block">{name}</Section>
+
+	{#if bio}
+		<Text class="bioText">{bio}</Text>
+	{/if}
 </div>
 
+<div class="sectionMarker"></div>
+
 <style lang="postcss">
+	.sectionMarker {
+		width: 90%;
+		margin: 0 auto;
+		border-bottom: 1px solid #999999;
+	}
 </style>
