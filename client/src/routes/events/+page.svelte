fc265ab96a3f02ca15af2b3734902a7f12caa17c
Gurinder Bhogal <73514401+Gurinder45@users.noreply.github.com>
2024-08-16 18:03:48 -0700

    Events Page Set Up and Layout (#5)
    * Events page set up and layout
    
    * Respond to feedback
    
    ---------
    
    Co-authored-by: Sajan Toor <sajantoor98@gmail.com>

---
+++ client/src/routes/events/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import EventPage from '$lib/Components/EventPage/EventPage.svelte';
+</script>
+
+<EventPage
+	eventTitle="Science World Children's Day"
+	eventImage="https://assets.simpleviewinc.com/simpleview/image/upload/crm/vancouverbc/Science-World-03-large_7A18828C-0A13-47CD-92DB4404AE5A222C_fb32edf9-a996-4f5f-a2aaa1980fe8d6b7.jpg"
+	tags={['tag1', 'tag2', 'tag3']}
+	files={['file1', 'file2', 'file3']}
+	spotsAvailable={10}
+	startDate={new Date('2024-01-07')}
+	endDate={new Date('2024-01-09')}
+	location="Vancouver, BC 1455 Quebec St"
+	hours="10am - 5pm"
+	hostImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT46Pp3V6MJmwjab7ghq7TSOu5COQongTQ83Q&s"
+	hostName="The City of Vancouver"
+	eventDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan purus vel elit scelerisque faucibus. Vestibulum et suscipit ante. Ut quam elit, mollis vel justo sit amet, viverra varius elit. Pellentesque at augue nec dolor viverra laoreet. Nulla mattis felis ligula, id consectetur nisl ornare at. Morbi bibendum ante non dolor posuere commodo. Maecenas at felis diam. Quisque fermentum auctor ullamcorper. In nunc sapien, consectetur vitae interdum nec, ultrices ac nisl. Vivamus rutrum leo felis, vel maximus neque luctus vitae. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque ac iaculis mi. Morbi eros est, condimentum vel ligula sit amet, iaculis eleifend diam. Sed pretium arcu at."
+	preShiftInfo="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan purus vel elit scelerisque faucibus. Vestibulum et suscipit ante. Ut quam elit, mollis vel justo sit amet, viverra varius elit. Pellentesque at augue nec dolor viverra laoreet. Nulla mattis felis ligula, id consectetur nisl ornare at. Morbi bibendum ante non dolor posuere commodo. Maecenas at felis diam."
+/>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {},
+		colors: {
+			primaryYellow: '#FABD22',
+			altTextGray: '#999999',
+			tagYellow: '#FFE6A6',
+			placeholderGray: "#F5F5F5"
+		}
+	},
+	plugins: []
+};
