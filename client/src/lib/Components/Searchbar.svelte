4192118ed15ddfb3c1b64c55c5beab7ffb9a994e
JagdeepMinhas <128650390+JagdeepMinhas@users.noreply.github.com>
2024-08-20 18:31:00 -0700

    Created Search bar and the Dashboard Page (#10)
    * Created Searchbar and the dashboard page
    * Slight tweaks
    * Fix events scroller
    
    ---------
    
    Co-authored-by: Jagdeep <jsa397@sfu.ca>
    Co-authored-by: Sajan Toor <sajantoor98@gmail.com>

---
+++ client/src/lib/Components/Searchbar.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import SearchIcon from 'svelte-material-icons/Magnify.svelte';
+
+	let searchQuery = '';
+	let iconSize = 22;
+
+	function handleSearch() {
+		console.log('Search query:', searchQuery);
+	}
+</script>
+
+<div class="search-bar">
+	<input
+		type="text"
+		placeholder="Search events"
+		bind:value={searchQuery}
+		on:keydown={(e) => e.key === 'Enter' && handleSearch()}
+	/>
+	<button class="icon-container" on:click={handleSearch}>
+		<SearchIcon size={iconSize} />
+	</button>
+</div>
+
+<style>
+	.search-bar {
+		display: flex;
+		align-items: center;
+		width: 90%;
+		margin: 0 auto;
+		position: relative;
+	}
+
+	input {
+		flex: 1;
+		padding: 0.5rem 2.5rem 0.5rem 1rem; /* Add padding-right to make space for the icon */
+		background-color: #f4f4f4;
+		border-radius: 6px;
+		outline: none;
+	}
+
+	.icon-container {
+		position: absolute;
+		right: 0.5rem;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background-color: #f4f4f4;
+		border-radius: 6px;
+		padding: 0.25rem;
+		cursor: pointer;
+	}
+</style>
--- client/src/routes/dashboard/+page.svelte
+++ client/src/routes/dashboard/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-	import EventScroller from '$lib/Components/EventScroller.svelte';
 	import NavBar from '$lib/Components/NavBar.svelte';
+	import EventScroller from '$lib/Components/EventScroller.svelte';
 	import CalendarView from '$lib/Components/CalendarView.svelte';
-	
+	import Searchbar from '$lib/Components/Searchbar.svelte';
+	import Text from '$lib/Components/Text/Text.svelte';
+
 	import type { Event } from '$lib/Types/Events';
 
 	let events: Event[] = [
@@ -67,9 +69,51 @@
 			location: 'Vancouver, BC 1455 Quebec St'
 		}
 	];
+
+	let tags: string[] = ['Childcare', 'Education', 'Event Planning'];
 </script>
 
-<NavBar />
+<div class="dashboard mb-8">
+	<NavBar />
+	<Text class="text-2xl font-bold ml-[5%] mt-[1%] mb-[1%]">Events</Text>
+	<Searchbar />
 
-<EventScroller {events} />
-<CalendarView />
+	{#if tags && tags.length > 0}
+		<div class="tags-section mt-[2%] mb-4 ml-[5%]">
+			<div class="tag-list mt-2">
+				{#each tags as tag}
+					<div class="tag bg-tagYellow">
+						<Text>{tag}</Text>
+					</div>
+				{/each}
+			</div>
+		</div>
+	{/if}
+	<Text class="text-2xl font-bold ml-[5%] mt-[1%] mb-[1%]">Featured Events</Text>
+
+	<EventScroller {events} />
+	<CalendarView />
+</div>
+
+<style lang="postcss">
+	.dashboard {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+
+	.tag-list {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 1.5rem;
+	}
+
+	.tag {
+		padding: 0.5rem 1rem;
+		min-width: 6rem;
+		border-radius: 4rem;
+		display: inline-block;
+		white-space: nowrap;
+		text-align: center;
+	}
+</style>
