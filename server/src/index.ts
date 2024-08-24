16f5087fb1741ea3ab1b015be1e5099dcbb1a8c7
Gurinder Bhogal <73514401+Gurinder45@users.noreply.github.com>
2024-08-23 17:09:39 -0700

    Users CRUD APIs (#12)

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import db, { connectDB } from "./database/db";
 import { createEvent, getEvent, getEvents, updateEvent } from "./routes/events";
 import { getOrganizations, getOrganization, createOrganization, updateOrganization, deleteOrganization } from "./routes/organizations";
+import { getUser, createUser, updateUser, deleteUser } from "./routes/user";
+
 const app = express();
 const port = 4000;
 connectDB();
@@ -18,6 +20,7 @@ app.get("/health", (req, res) => {
 const API_PREFIX = "/api/v1";
 const EVENTS_API = `${API_PREFIX}/events`;
 const ORGANIZATIONS_API = `${API_PREFIX}/organizations`;
+const USERS_API = `${API_PREFIX}/users`;
 
 app.get(`${EVENTS_API}`, (req, res) => getEvents(req, res));
 app.get(`${EVENTS_API}/:id`, (req, res) => getEvent(req, res));
@@ -29,3 +32,8 @@ app.get(`${ORGANIZATIONS_API}/:id`, getOrganization);
 app.post(`${ORGANIZATIONS_API}`, createOrganization);
 app.patch(`${ORGANIZATIONS_API}/:id`, updateOrganization);
 app.delete(`${ORGANIZATIONS_API}/:id`, deleteOrganization);
+
+app.get(`${USERS_API}/:id`, getUser);
+app.post(`${USERS_API}`, createUser);
+app.patch(`${USERS_API}/:id`, updateUser);
+app.delete(`${USERS_API}/:id`, deleteUser);
---
+++ server/src/routes/user.ts
@@ -0,0 +1,85 @@
+import { Request, Response } from "express";
+import User from "../database/models/user";
+
+interface UserBody {
+    name: string;
+    email: string;
+    phoneNumber: string;
+    password: string;
+    dateOfBirth: Date;
+    emergencyContact: {
+        name: string;
+        phoneNumber: string;
+        relationship: string;
+    };
+    registeredEvents?: string[];
+}
+
+export async function getUser(req: Request, res: Response) {
+    const userId = req.params.id;
+    if (!userId) {
+        return res.status(400).json({ message: "Invalid user id" });
+    }
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        return res.status(200).json(user);
+    } catch (error) {
+        return res.status(400).json({ message: "Invalid user id" });
+    }
+}
+
+export async function createUser(req: Request, res: Response) {
+    const userBody = req.body as UserBody;
+    if (!userBody) {
+        return res.status(400).json({ message: "Invalid user body" });
+    }
+
+    if (!userBody.name || !userBody.email || !userBody.phoneNumber || !userBody.password || !userBody.dateOfBirth || !userBody.emergencyContact) {
+        return res.status(400).json({ message: 'Missing required fields' });
+    }
+
+    try {
+        const newUser = new User(userBody);
+        const savedUser = await newUser.save();
+        return res.status(201).json(savedUser);
+    } catch (error) {
+        return res.status(500).json({ message: "Error creating user", error });
+    }
+}
+
+export async function updateUser(req: Request, res: Response) {
+    const userId = req.params.id;
+    const userBody = req.body as UserBody;
+    if (!userId || !userBody) {
+        return res.status(400).json({ message: "Invalid user id or body" });
+    }
+    try {
+        const user = await User.findByIdAndUpdate(userId, userBody, { new: true });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        return res.status(200).json(user);
+    } catch (error) {
+        return res.status(400).json({ message: "Invalid user id" });
+    }
+}
+
+export async function deleteUser(req: Request, res: Response) {
+    const userId = req.params.id;
+    if (!userId) {
+        return res.status(400).json({ message: "Invalid user id" });
+    }
+    try {
+        const user = await User.findByIdAndDelete(userId);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        return res.status(200).json({ message: "User deleted successfully" });
+    } catch (error) {
+        return res.status(400).json({ message: "Invalid user id" });
+    }
+}
+
