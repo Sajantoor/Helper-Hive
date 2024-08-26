170b95ddcfbd41e0949546e82b2fc214321134df
GurbirSD <84153647+GurbirSD@users.noreply.github.com>
2024-08-26 10:16:31 -0700

    Registration, Login, Forgot Password Pages (#15)
    * Registration for org and volunteer
    
    * Registration and Login pages + CSS
    
    * Replace all text with <Text> tags
    
    * Input validation and password peaking
    
    Going to use plugins for DOB and phone number tomorrow, current is V1 functionality
    
    * Input field components and date plugin
    
    * Clean up form logic
    
    * Tel-Input implementation
    
    * Terms&Conditions and Forgot Password
    
    * Formatting + enable typescript
    
    ---------
    
    Co-authored-by: Jessy Chahal <jessychahal432@gmail.com>
    Co-authored-by: Sajan Toor <sajantoor98@gmail.com>

---
+++ client/src/lib/Components/TCContents.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	export let type = 'terms';
+
+	const terms = `
+    <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text><br>
+    <Text>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text><br>
+    <Text>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</Text><br>
+    <Text>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text><br>
+    <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text><br>
+    <Text>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text><br>
+    <Text>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</Text><br>
+    <Text>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text><br>
+    <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text><br>
+    <Text>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text><br>
+    <Text>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</Text><br>
+    <Text>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text><br>
+    <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text><br>
+    <Text>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text><br>
+    <Text>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</Text><br>
+    <Text>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+	<Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text><br>
+    <Text>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text><br>
+    <Text>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</Text><br>
+    <Text>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text><br>
+    <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text><br>
+    <Text>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text><br>
+    <Text>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</Text><br>
+    <Text>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text><br>
+    <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text><br>
+    <Text>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text><br>
+    <Text>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</Text><br>
+    <Text>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text><br>
+    <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text><br>
+    <Text>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text><br>
+    <Text>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</Text><br>
+    <Text>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+  `;
+
+	const liabilityOrg = `
+    <Text>Organizational liability content goes here...</Text>
+  `;
+
+	const liabilityVol = `
+    <Text>Volunteer liability content goes here...</Text>
+  `;
+
+	let content = '';
+
+	if (type === 'terms') {
+		content = terms;
+	} else if (type === 'liabilityOrg') {
+		content = liabilityOrg;
+	} else if (type === 'liabilityVol') {
+		content = liabilityVol;
+	}
+</script>
+
+<div>
+	{@html content}
+</div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,13 +2,21 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {},
-		colors: {
-			primaryYellow: '#FABD22',
-			altTextGray: '#999999',
-			tagYellow: '#FFE6A6',
-			placeholderGray: "#F5F5F5"
-		}
+		extend: {
+			screens: {
+				'mdlg': '800px', // Custom breakpoint between md and lg
+			},
+			colors: {
+				primaryYellow: '#FABD22',
+				altTextGray: '#999999',
+				altTextBrown: '#A98D36',
+				tagYellow: '#FFE6A6',
+				placeholderGray: "#F5F5F5",
+				placeholderGrayText: "#7A7A7A",
+				darkGray: '#373737',
+				dimBlack: '#646464'
+			}
+		},
 	},
 	plugins: []
 };
