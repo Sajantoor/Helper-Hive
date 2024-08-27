9cd97ce45d9feecb15a7754725bf26c82d637456
JagdeepMinhas <128650390+JagdeepMinhas@users.noreply.github.com>
2024-08-26 17:38:50 -0700

    Registration APIs (#16)
    * Registration Api
    
    * added endpoint for deregistration
    
    ---------
    
    Co-authored-by: Jagdeep <jsa397@sfu.ca>

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,7 @@ import db, { connectDB } from "./database/db";
 import { createEvent, getEvent, getEvents, updateEvent } from "./routes/events";
 import { getOrganizations, getOrganization, createOrganization, updateOrganization, deleteOrganization } from "./routes/organizations";
 import { getUser, createUser, updateUser, deleteUser } from "./routes/user";
-
+import { getUserFutureEvents, getUserAllEvents, getOrganizationEvents, registerForEvent, deregisterForEvent } from "./routes/registration";
 const app = express();
 const port = 4000;
 connectDB();
@@ -21,6 +21,7 @@ const API_PREFIX = "/api/v1";
 const EVENTS_API = `${API_PREFIX}/events`;
 const ORGANIZATIONS_API = `${API_PREFIX}/organizations`;
 const USERS_API = `${API_PREFIX}/users`;
+const REGISTRATION_API = `${API_PREFIX}/registrations`; 
 
 app.get(`${EVENTS_API}`, (req, res) => getEvents(req, res));
 app.get(`${EVENTS_API}/:id`, (req, res) => getEvent(req, res));
@@ -37,3 +38,9 @@ app.get(`${USERS_API}/:id`, getUser);
 app.post(`${USERS_API}`, createUser);
 app.patch(`${USERS_API}/:id`, updateUser);
 app.delete(`${USERS_API}/:id`, deleteUser);
+
+app.get(`${REGISTRATION_API}/user/:id/future-events`, getUserFutureEvents);
+app.get(`${REGISTRATION_API}/user/:id/all-events`, getUserAllEvents); // we can rename this to getPresentAndPastEvents
+app.get(`${REGISTRATION_API}/organization/:id/events`, getOrganizationEvents);
+app.post(`${REGISTRATION_API}/register`, registerForEvent);
+app.post(`${REGISTRATION_API}/deregister`, deregisterForEvent);
