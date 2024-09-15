71b742bc2f11d0d2cdae66f5df5935b1dc257067
Sajan Toor <sajantoor98@gmail.com>
2024-09-14 23:07:43 -0700

    Small tweaks

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -20,11 +20,11 @@
 	</div>
 
 	<div class="flex items-center gap-5">
-		<Text class="ml-5">Home</Text>
+		<!-- <Text class="ml-5">Home</Text>
 		<Text class="ml-5">About Us</Text>
 		<Text class="ml-5">Our Team</Text>
 		<Text class="ml-5">Sponsor</Text>
-		<Text class="ml-5">Contact</Text>
+		<Text class="ml-5">Contact</Text> -->
 
 		<a href="/login">
 			<button class="ml-5 bg-transparent border border-black py-2 px-4 rounded-full">
@@ -36,14 +36,14 @@
 
 <div class="join-container">
 	<LargeHeading>Start Volunteering Today!</LargeHeading>
-	<Text class="w-1/3 text-center mt-2"
-		>Dive into the hive of local opportunities, pick what sparks your curiosity, and join in—it’s as
-		easy as gathering nectar!</Text
-	>
+	<Text class="w-1/3 text-center mt-2">
+		Dive into the hive of local opportunities, pick what sparks your curiosity, and join in—it's as
+		easy as gathering nectar!
+	</Text>
 	<a href="/registration/volunteer">
-		<button class="bg-primaryYellow text-black py-2 px-4 mt-8 rounded-lg"
-			><Heading>JOIN NOW</Heading></button
-		>
+		<button class="bg-primaryYellow text-black py-2 px-4 mt-8 rounded-lg">
+			<Heading>JOIN NOW</Heading>
+		</button>
 	</a>
 </div>
 
@@ -70,7 +70,6 @@
 
 <div class="mb-8">
 	<Section class="text-center mb-8">Upcoming Events</Section>
-
 	<EventScroller {events} />
 </div>
 
@@ -78,59 +77,59 @@
 	<Section class="text-center">Join a Community</Section>
 
 	<div class="flex justify-center">
-		<LargeText class="text-center w-5/6 mt-8"
+		<Text class="text-center w-5/6 mt-8"
 			>Join our hive, where volunteers and organizations come together for more than just service.
 			Experience streamlined volunteering, build lasting community connections, and network with
-			local leaders. Together, let's create a buzzing new volunteer community!</LargeText
-		>
+			local leaders. Together, let's create a buzzing new volunteer community!
+		</Text>
 	</div>
 </div>
 
 <div>
 	<Section class="text-center">Frequently Asked Questions</Section>
 	<div class="faq-item">
-		<Section>What is Helper Hive</Section>
-		<LargeText
+		<LargeText class="font-bold">What is Helper Hive?</LargeText>
+		<Text
 			>Helper Hive Volunteer Network is a vibrant community hub where you can easily find flexible
 			drop-in volunteer opportunities that fit into your busy schedule. Our platform offers a
 			variety of unique roles with respected organizations, removing the hassle of lengthy
 			onboarding and training. At Helper Hive, you can engage freely and flexibly in activities that
 			spark your passion, whenever it suits you. Explore local opportunities, sign up effortlessly,
-			and bring along your friends, family, and neighbors. Together, let’s BEE more involved and
-			make a positive impact in our community! Visit our About Us page to learn more.</LargeText
-		>
+			and bring along your friends, family, and neighbors. Together, let's BEE more involved and
+			make a positive impact in our community!
+		</Text>
 	</div>
 	<div class="faq-item">
-		<Section>Who can join?</Section>
-		<LargeText
+		<LargeText class="font-bold">Who can join?</LargeText>
+		<Text
 			>Helper Hive Volunteer Network warmly welcomes individuals from all walks of life to join our
 			community. We believe that everyone has unique skills and perspectives to offer, and we
 			encourage people of all ages and backgrounds to sign up and contribute. For children under the
 			age of 16, we require parental permission to ensure a supportive and safe volunteer
-			experience. By embracing diversity and inclusivity, we strive to create meaningful
-			opportunities for all to engage in service and make a positive impact together.</LargeText
-		>
+			experience. By embracing diversity and inclusively, we strive to create meaningful
+			opportunities for all to engage in service and make a positive impact together.
+		</Text>
 	</div>
 	<div class="faq-item">
-		<Section>How do I join?</Section>
-		<LargeText
+		<LargeText class="font-bold">How do I join?</LargeText>
+		<Text
 			>Joining Helper Hive is easy! Just click the "Register" or "Login" buttons at the top of the
 			page to get started. You'll need to create an account using a valid email address, and once
-			your email is verified, you can optionally set up your user profile. After that, you’ll have
-			full access to all the opportunities on our network. It’s that simple—sign up and you’re ready
-			to dive into your volunteering journey!</LargeText
-		>
+			your email is verified, you can optionally set up your user profile. After that, you'll have
+			full access to all the opportunities on our network. It's that simple—sign up and you're ready
+			to dive into your volunteering journey!
+		</Text>
 	</div>
 	<div class="faq-item">
-		<Section>Will my hours be recorded?</Section>
-		<LargeText
+		<LargeText class="font-bold">Will my hours be recorded?</LargeText>
+		<Text
 			>Your volunteer hours will be automatically tracked on our website for your convenience. Once
 			you sign up for an event, you can view your pending hours on your personal user dashboard.
 			After your hours are approved, they will be updated to a verified status and added to your
-			total count. You’ll see a comprehensive breakdown of your hours, including the total completed
+			total count. You'll see a comprehensive breakdown of your hours, including the total completed
 			on Helper Hive Network, as well as a detailed view by organization and event. This system
-			ensures you have a clear and accurate record of all your volunteering activities.</LargeText
-		>
+			ensures you have a clear and accurate record of all your volunteering activities.
+		</Text>
 	</div>
 </div>
 
@@ -181,7 +180,7 @@
 	.hexagons {
 		display: flex;
 		justify-content: center;
-		margin-top: 12rem;
+		margin-top: 4rem;
 		gap: 4rem; /* Space between hexagons */
 	}
 
