ce69d79b2371058bb4304d789c2996d9ec1a56c6
Sajan Toor <sajantoor98@gmail.com>
2024-09-20 19:08:44 -0700

    Redirect to app if logged in
    * Add favicon
    * Fix outdated types in page.server.ts

--- client/src/routes/app/+layout.server.ts
+++ client/src/routes/app/+layout.server.ts
@@ -1,23 +1,16 @@
-import { PUBLIC_SERVER_HOST } from '$env/static/public';
 import type { ProfileResponse } from '$common/types/ProfileResponse';
 import { handleErrors } from '$lib/Utils/handleErrors';
 import type { LayoutServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
+import { fetchProfile } from '$lib/Utils/fetch';
 
 export const load: LayoutServerLoad = async ({ cookies }) => {
     // if cookies aren't there, redirect to login page 
-    if (!cookies.get("id") || !cookies.get("rid")) {
-        redirect(302, "/login")
+    if (!cookies.get("rid") || !cookies.get("id")) {
+        redirect(302, "/login");
     }
 
-    const response = await fetch(`${PUBLIC_SERVER_HOST}/api/profile`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Cookie': `id=${cookies.get("id")}; rid=${cookies.get("rid")}`,
-        },
-        credentials: 'include',
-    });
+    const response = await fetchProfile(cookies);
 
     if (!response.ok) {
         await handleErrors(response);
