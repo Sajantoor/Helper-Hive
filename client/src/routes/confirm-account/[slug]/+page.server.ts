3cae60512f3fb2d6e6c5a9b0506fab584e91fe98
Sajan Toor <sajantoor98@gmail.com>
2024-09-24 18:02:32 -0700

    Propagate account confirmation cookies to client

--- client/src/routes/confirm-account/[slug]/+page.server.ts
+++ client/src/routes/confirm-account/[slug]/+page.server.ts
@@ -1,7 +1,8 @@
 import { PUBLIC_SERVER_HOST } from "$env/static/public";
 import { handleErrors } from "$lib/Utils/handleErrors";
+import { splitCookiesString, parseString } from "set-cookie-parser";
 
-export async function load({ params }) {
+export async function load({ params, cookies }) {
     const response = await fetch(`${PUBLIC_SERVER_HOST}/api/confirm-account/${params.slug}`, {
         method: 'GET',
         headers: {
@@ -12,6 +13,24 @@ export async function load({ params }) {
 
     if (!response.ok) {
         await handleErrors(response);
+    }
+
+    // TODO: this is hacky fix and should be fixed later
+    // this is a hack to get the response cookies to propagate to the client 
+    const setCookieHeader = response.headers.get('set-cookie');
+
+    if (setCookieHeader) {
+        for (const str of splitCookiesString(setCookieHeader)) {
+            const { name, value, ...options } = parseString(str);
+
+            const parsedOptions = {
+                ...options,
+                path: options.path || '/',
+                sameSite: options.sameSite as "lax" | "strict" | "none" | undefined,
+            };
+
+            cookies.set(name, value, parsedOptions);
+        }
     }
 
     return;
