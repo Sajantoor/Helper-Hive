dc0b6344786e710fdff69cc8e96a6a90943cb37b
Sajan Toor <sajantoor98@gmail.com>
2024-09-28 18:42:30 -0700

    Forward cookies from server to client using hook

--- client/src/hooks.server.ts
+++ client/src/hooks.server.ts
@@ -1,10 +1,17 @@
-import { PUBLIC_SERVER_DOMAIN, PUBLIC_SERVER_HOST } from "$env/static/public";
-import type { Handle } from "@sveltejs/kit";
-const { PROD } = import.meta.env;
+import { PUBLIC_SERVER_HOST } from "$env/static/public";
+import type { HandleFetch, RequestEvent } from "@sveltejs/kit";
+import { splitCookiesString, parseString } from "set-cookie-parser";
 
-export const handle: Handle = async ({ event, resolve }) => {
-    if (event.cookies.get("rid")) {
-        // try to refresh the token 
+export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
+    let response = await fetch(request);
+
+    if (!request.url.startsWith(PUBLIC_SERVER_HOST)) {
+        return response;
+    }
+
+    // If we're making a request to the server and it's a 401, we may need to 
+    // refresh the token. Refresh the token and retry the request. 
+    if (response.status === 401 && event.cookies.get("rid") === null) {
         const tokenFetch = await fetch(`${PUBLIC_SERVER_HOST}/api/token`, {
             method: 'POST',
             headers: {
@@ -15,21 +22,30 @@ export const handle: Handle = async ({ event, resolve }) => {
         });
 
         if (!tokenFetch.ok) {
-            return resolve(event);
+            return response;
         }
 
-        const token = await tokenFetch.json();
-
-        // TODO: make sure this is the same as the server
-        event.cookies.set("id", token.accessToken, {
-            httpOnly: true,
-            secure: PROD,
-            sameSite: PROD ? "none" : "lax",
-            path: "/",
-            domain: PROD ? PUBLIC_SERVER_DOMAIN : "",
-            maxAge: 60 * 60 * 24 * 7, // 7 days
-        });
+        response = await fetch(request);
     }
 
-    return resolve(event);
+    setCookies(response, event);
+    return response;
+}
+
+const setCookies = (response: Response, event: RequestEvent<Partial<Record<string, string>>, string | null>) => {
+    const setCookieHeader = response.headers.get('set-cookie');
+
+    if (setCookieHeader) {
+        for (const str of splitCookiesString(setCookieHeader)) {
+            const { name, value, ...options } = parseString(str);
+
+            const parsedOptions = {
+                ...options,
+                path: options.path || '/',
+                sameSite: options.sameSite as "lax" | "strict" | "none" | undefined,
+            };
+
+            event.cookies.set(name, value, parsedOptions);
+        }
+    }
 }
--- client/src/routes/confirm-account/[slug]/+page.server.ts
+++ client/src/routes/confirm-account/[slug]/+page.server.ts
@@ -1,8 +1,7 @@
 import { PUBLIC_SERVER_HOST } from "$env/static/public";
 import { handleErrors } from "$lib/Utils/handleErrors";
-import { splitCookiesString, parseString } from "set-cookie-parser";
 
-export async function load({ params, cookies }) {
+export async function load({ params }) {
     const response = await fetch(`${PUBLIC_SERVER_HOST}/api/confirm-account/${params.slug}`, {
         method: 'GET',
         headers: {
@@ -13,24 +12,6 @@ export async function load({ params, cookies }) {
 
     if (!response.ok) {
         await handleErrors(response);
-    }
-
-    // TODO: this is hacky fix and should be fixed later
-    // this is a hack to get the response cookies to propagate to the client 
-    const setCookieHeader = response.headers.get('set-cookie');
-
-    if (setCookieHeader) {
-        for (const str of splitCookiesString(setCookieHeader)) {
-            const { name, value, ...options } = parseString(str);
-
-            const parsedOptions = {
-                ...options,
-                path: options.path || '/',
-                sameSite: options.sameSite as "lax" | "strict" | "none" | undefined,
-            };
-
-            cookies.set(name, value, parsedOptions);
-        }
     }
 
     return;
